6be8878270f5bf113c69744e97c8f3a06b15b4c8
unknown <aksjd@qq.com>
2022-06-27 22:51:17 +0800

    1

---
+++ myone/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HELLO WORLD!");
+           // printf（"%f\n"666）
+        }
+    }
+}
